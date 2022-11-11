3487de44341723638f657bf4ba633b122ea44763
Emna <84625792+Emna747@users.noreply.github.com>
2022-11-11 19:09:08 +0200

    bootstrap updates

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import Popper from "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./components/App";
+import Button from "react-bootstrap/Button";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
