82789a72ced4fcb47990eceda8312190801305c3
Emna <84625792+Emna747@users.noreply.github.com>
2022-11-14 19:04:35 +0200

    CSS updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,15 @@ import React from "react";
 //basically card which is the componenet is the props within the function
 function Card(props) {
   return (
-    <div className="Mainboard">
-      <div className="Jobcard">
-        <h3 className="thejobtitle">{props.JobTitle}</h3>
-        <h6 className="thedepartment">{props.Department}</h6>
-        <h6 className="thelocation">{props.Location}</h6>
-        <p className="thedescription">{props.Description}</p>
-        <p className="thesalary">{props.Salary}</p>
+    <div className="Main">
+      <div className="Mainboard">
+        <div className="Jobcard">
+          <h3 className="thejobtitle">{props.JobTitle}</h3>
+          <h6 className="thedepartment">{props.Department}</h6>
+          <h6 className="thelocation">{props.Location}</h6>
+          <p className="thedescription">{props.Description}</p>
+          <p className="thesalary">{props.Salary}</p>
+        </div>
       </div>
     </div>
   );
